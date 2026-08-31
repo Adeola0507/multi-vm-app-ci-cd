432d5b0ac5103516026ab55d28aaf6028c7961b0
Adeola0507 <adechulababe@gmail.com>
2026-08-31 19:37:22 +0100

    fix startup script

--- Terraform/startup-script.sh
+++ Terraform/startup-script.sh
@@ -2,27 +2,26 @@
 set -x
 exec > /var/log/startup-script.log 2>&1
 
-# Install Docker
 apt-get update -y
 apt-get install -y ca-certificates curl gnupg git
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update -y
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 systemctl enable docker
 systemctl start docker
-usermod -aG docker debian # <- important
+sleep 15
 
-# Clone the repo and bring the stack up
+# Add swap so build doesn't get OOM killed
+fallocate -l 2G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+
 cd /opt
 git clone https://github.com/Adeola0507/multi-vm-app-ci-cd.git ci-teaching-kit
 cd ci-teaching-kit
 docker compose up -d --build
-
-echo "CI Stack deployed. Nexus:8081 Jenkins:8080 API:3000"
